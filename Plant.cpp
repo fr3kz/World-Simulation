26710f0abe94206fea50d541ba715000c3c98310
Artur Kunicki <tektmap1@gmail.com>
2024-01-14 22:06:45 +0100

    Init

---
+++ Plant.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Artur on 14/01/2024.
+//
+
+#include "Plant.h"
+
+void Plant::grow() {
+    energy+=2;
+}
---
+++ Plant.h
@@ -0,0 +1,19 @@
+//
+// Created by Artur on 14/01/2024.
+//
+
+#ifndef EVOLUTION_PLANT_H
+#define EVOLUTION_PLANT_H
+
+
+class Plant {
+
+public:
+    void grow();
+
+private:
+    int energy;
+};
+
+
+#endif //EVOLUTION_PLANT_H
